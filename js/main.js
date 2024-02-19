81c1547e256f7acd0fdde8e1bedf0008c1e1c84d
IGNATOV93 <ignatowja@gmail.com>
2024-02-20 00:49:44 +0300

    fix error play video ('#video-story') z-index!=auto

--- js/main.js
+++ js/main.js
@@ -1,18 +1,19 @@
 // =============== MOBILE NAV =================
-  const mobileNavButton = document.querySelector('.mobile-nav-button');
-  const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
-  const mobileNav = document.querySelector('.mobile-nav');
-  mobileNavButton.addEventListener('click',function() {
-    mobileNavIcon.classList.toggle('active');
-    mobileNav.classList.toggle('active');
-    document.body.classList.toggle('no-scroll');
-  });
+const mobileNavButton = document.querySelector('.mobile-nav-button');
+const mobileNavIcon = document.querySelector('.mobile-nav-button__icon');
+const mobileNav = document.querySelector('.mobile-nav');
+mobileNavButton.addEventListener('click',function() {
+  mobileNavIcon.classList.toggle('active');
+  mobileNav.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
+});
 
   // =============== VIDEO NAV =================
   
-  const videoBtn = document.querySelector('#video-story-btn');
-  const videoFile = document.querySelector('#video-story');
-  videoBtn.addEventListener('click', function() {
-   videoFile.play();
-  });
-
+ 
+    const videoBtn = document.querySelector('#video-story-btn');
+    const videoFile = document.querySelector('#video-story');
+  
+      videoBtn.addEventListener('click', function() {
+       videoFile.play();
+      })
