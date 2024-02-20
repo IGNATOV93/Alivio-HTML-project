d9fb0a60f2bcfbff9651e5cbaa40534263814614
IGNATOV93 <ignatowja@gmail.com>
2024-02-21 01:24:04 +0300

    Done this project

--- js/main.js
+++ js/main.js
@@ -12,8 +12,31 @@ mobileNavButton.addEventListener('click',function() {
   
  
     const videoBtn = document.querySelector('#video-story-btn');
+    const videoBtnIcon = document.querySelector('#video-story-btn-icon');
+    const videoOverlay = document.querySelector('#video-story-overlay');
     const videoFile = document.querySelector('#video-story');
-  
+    
       videoBtn.addEventListener('click', function() {
-       videoFile.play();
+        function toggleOverlay(event){
+          if(event.type==="mouseleave"){
+            videoOverlay.classList.add('hidden');
+          }
+          else{
+            videoOverlay.classList.remove('hidden');
+          }
+        }
+        if(videoFile.paused){
+          videoFile.play();
+          videoBtnIcon.src='./img/story/pause_white..svg';
+           videoOverlay.onmouseleave = toggleOverlay;
+           videoOverlay.onmouseenter = toggleOverlay;
+        }
+        else
+        {
+          videoFile.pause();
+          videoBtnIcon.src='./img/story/play_white.svg';
+          videoOverlay.onmouseleave=null;
+          videoOverlay.onmouseenter = null;
+        }
+       
       })
